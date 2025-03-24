02a7c2de75bc0032f91b4058e417653418ed6ea0
kartik132000 <sujeetkumarsen37@gmail.com>
2025-03-24 15:49:05 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,52 @@
-import React from "react";
-import './../styles/App.css';
-import WeatherDisplay from "./WeatherDisplay"; // Corrected import
 
-const App = () => {
-  const [data, setData] = React.useState(null);
+import React, { Component, useState } from "react";
+import '../styles/App.css';
 
-  React.useEffect(() => {
-    setData({ temperature: 25, conditions: "Sunny" });
-  }, []);
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            renderBall: false,
+            posi : 5,
+        };
+        this.renderChoice = this.renderBallOrButton.bind(this)
+        this.buttonClickHandler = this.buttonClickHandler.bind(this)
+	     this.onMove = this.onMove.bind(this);
+    };
 
-  return (
-    <div>
-      {data && <WeatherDisplay data={data} />} {/* Corrected component name */}
-    </div>
-  );
-};
+    buttonClickHandler() {
+	   this.setState({renderBall:true})
+   }
+    renderBallOrButton() {
+		if (this.state.renderBall) {
+		    return <div className="ball" style={{left:this.state.posi}}></div>
+		} else {
+		    return <button className="start" onClick={this.buttonClickHandler} >Start</button>
+		}
+    }
+onMove() {
+    this.setState({ posi:this.state.posi+5 });
+  }
+
+    // bind ArrowRight keydown event
+    componentDidMount() {
+	    const onMove = this.onMove
+       window.addEventListener("keydown", function (e) {
+      const key = e.key;
+      if (key === "ArrowRight") {
+        onMove();
+      }
+    });
+    }
+
+    render() {
+        return (
+            <div className="playground">
+                {this.renderBallOrButton()}
+            </div>
+        )
+    }
+}
+
 
 export default App;
